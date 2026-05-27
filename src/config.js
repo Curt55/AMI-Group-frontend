4c6996aaf14853210e363f64411eead83b967d86
Curt55 <103041218+Curt55@users.noreply.github.com>
2026-05-28 04:55:51 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+// frontend/src/config.js
+const API_URL = 'https://ami-group-production.up.railway.app';
+
 export default API_URL;
